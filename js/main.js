cfd9fe262ea1d6db4d66895bb7aa527ca0403347
Louayhj <139022326+Louayhj@users.noreply.github.com>
2024-03-03 18:05:46 +0200

    initialiazing variables and game

---
+++ js/main.js
@@ -0,0 +1,26 @@
+window.onload = function () {
+    const colors = ["green", "red", "blue", "yellow"];
+    const audioMap = {
+      green: "sounds/green.mp3",
+      red: "sounds/red.mp3",
+      blue: "sounds/blue.mp3",
+      yellow: "sounds/yellow.mp3",
+      wrong: "sounds/wrong.mp3",
+      gameover: "sounds/game-over.wav",
+      gamewin: "sounds/game-win.wav",
+    };
+  
+    let pattern = [];
+    let userPattern = [];
+    let level = 0;
+    let highScore = 0;
+  
+    const playButton = document.getElementById("play");
+    const infoText = document.getElementById("info");
+    const highScoreDisplay = document.getElementById("high-score");
+    const levelDisplay = document.getElementById("level");
+    const tiles = document.querySelectorAll(".tile");
+  
+   
+  };
+  
